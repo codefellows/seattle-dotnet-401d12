42e10fa6c48345bf6becd36734383f4705998a62
John Cokos <highlander.44@gmail.com>
2021-01-20 12:59:54 -0800

    Class 08 Demo uploaded

---
+++ class-08/demo/EnumsAndCollections/Classes/Date.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumsAndCollections.Classes
+{
+  class Date
+  {
+    public int DayOfMonth { get; set; }
+    public DayOfWeek Day { get; set; }
+  }
+
+  enum DayOfWeek
+  {
+    Sunday, // 0
+    Monday, // 1
+    Tuesday, // ...
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+  }
+
+  enum Months : byte
+  {
+    Jan,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+  }
+}
